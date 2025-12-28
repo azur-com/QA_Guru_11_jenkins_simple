e1f0e5709d8d0e074d3f4c8ce82a140c04bfbc2c
Alex-com <azurart@gamil.com>
2025-12-28 11:28:37 +0200

    second

---
+++ test.simple/test_fail.py
@@ -0,0 +1,15 @@
+
+def test_fail0():
+    asert = False
+    pass
+
+def test_fail1():
+    asert = False
+    pass
+
+def test_fail2():
+    asert = False
+    pass
+
+
+
---
+++ test.simple/test_pass.py
@@ -0,0 +1,12 @@
+
+def test_pass0():
+    pass
+
+def test_pass1():
+    pass
+
+def test_pass2():
+    pass
+
+
+
